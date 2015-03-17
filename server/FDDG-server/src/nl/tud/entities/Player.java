348a034b0ab8fccb2b75cfdc7c15e91408a4c833
lfdversluis <laurens-versluis@hotmail.com>
2015-03-17 18:32:44 +0100

    added comments to the Player class

--- server/FDDG-server/src/nl/tud/entities/Player.java
+++ server/FDDG-server/src/nl/tud/entities/Player.java
@@ -4,6 +4,13 @@ import java.util.Random;
 
 public class Player extends Unit {
 
+    /**
+     * Constructor of the player class. This initializes a new Player object
+     * and sets the basic parameters: x, y and id.
+     * @param x The initial x coordinate of the player in the grid.
+     * @param y The initial y coordinate of the player in the grid.
+     * @param id The unique ID of the player.
+     */
     public Player(int x, int y, int id) {
         Random random = new Random(System.currentTimeMillis());
         maxHitPoints = random.nextInt(11) + 10;
@@ -15,6 +22,11 @@ public class Player extends Unit {
         this.unitId = id;
     }
 
+    /**
+     * This function is called to heal the player up to its maximal amount
+     * of hit points.
+     * @param healHp The amount of hit points the user is healed with.
+     */
     public void heal(int healHp) {
         curHitPoints = Math.min(maxHitPoints, curHitPoints+healHp);
     }
