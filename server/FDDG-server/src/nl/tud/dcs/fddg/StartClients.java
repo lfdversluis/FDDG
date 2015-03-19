a467b222082198680b0335ef4520fc09f7204acf
Niels Spruit <niellus16@hotmail.com>
2015-03-19 11:04:18 +0100

    Now there is also a script to start a number of clients

--- server/FDDG-server/src/nl/tud/dcs/fddg/StartClients.java
+++ server/FDDG-server/src/nl/tud/dcs/fddg/StartClients.java
@@ -12,25 +12,30 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 /**
- * Start a client on the local machine
+ * Start one or more clients on the local machine
  * Created by Niels on 16-3-2015.
  */
-public class StartClient {
+public class StartClients {
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.err.println("Usage: StartClient <client ID>");
+        if (args.length < 2) {
+            System.err.println("Usage: StartClients <first client ID> <last client ID>");
             System.exit(1);
         }
 
         // parse arguments
-        int clientID = Integer.parseInt(args[0]);
+        int firstID = Integer.parseInt(args[0]);
+        int lastID = Integer.parseInt(args[1]);
 
-        // create the client process, bind it to the registry and start it
+        System.out.println("Starting " + (lastID - firstID + 1) + " clients...");
+
+        // create the client processes, bind them to the registry and start them
         try {
-            ClientProcess client = new ClientProcess(clientID);
-            Naming.rebind("FDDGClient/" + clientID, client);
-            new Thread(client).start();
+            for (int id = firstID; id <= lastID; id++) {
+                ClientProcess client = new ClientProcess(id);
+                Naming.rebind("FDDGClient/" + id, client);
+                new Thread(client).start();
+            }
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
