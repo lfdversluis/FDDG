cc3b90c4a7a304ada16e45105da04a2e6f15a439
Martijn de Vos <mdmartijndevos@gmail.com>
2015-03-22 23:46:33 +0100

    Added compilation of StartClients to compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 javac -cp ./server/FDDG-server/src server/FDDG-server/src/nl/tud/dcs/fddg/StartServer.java
+javac -cp ./server/FDDG-server/src server/FDDG-server/src/nl/tud/dcs/fddg/StartClients.java
