1116cb6c347e2c405e7835d66c16baa2a6462867
Niels Spruit <niellus16@hotmail.com>
2015-03-23 14:02:05 +0100

    Unacknowledged requests are now removed after 5s

--- server/FDDG-server/src/nl/tud/dcs/fddg/server/ServerProcess.java
+++ server/FDDG-server/src/nl/tud/dcs/fddg/server/ServerProcess.java
@@ -12,9 +12,7 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
@@ -40,6 +38,7 @@ public class ServerProcess extends UnicastRemoteObject implements ClientServerIn
     private Map<Integer, ServerInterface> otherServers; //(id, RMI object)
     private int requestCounter;
     private Map<Integer, ActionRequest> pendingRequests; //(requestID,request)
+    private Map<Integer,Timer> requestTimers; //(requestID, timer
     private Map<Integer, Integer> pendingAcknowledgements; //(requestID, nr of acks still to receive)
 
     /**
@@ -70,6 +69,7 @@ public class ServerProcess extends UnicastRemoteObject implements ClientServerIn
         this.requestCounter = 0;
         this.otherServers = new HashMap<Integer, ServerInterface>();
         this.pendingRequests = new HashMap<Integer, ActionRequest>();
+        this.requestTimers = new HashMap<Integer, Timer>();
         this.pendingAcknowledgements = new HashMap<Integer, Integer>();
 
         // start GUI if necessary
@@ -220,12 +220,21 @@ public class ServerProcess extends UnicastRemoteObject implements ClientServerIn
      */
     private void sendRequestsForAction(Action action) throws RemoteException {
         //create request
-        ActionRequest request = new ActionRequest(requestCounter++, this.ID, action);
+        final ActionRequest request = new ActionRequest(requestCounter++, this.ID, action);
 
         //initialize acknowledgement counter for the request
         pendingRequests.put(request.getRequestID(), request);
         pendingAcknowledgements.put(request.getRequestID(), otherServers.size());
-        //TODO: remove request from these data structures when not all acks are received after a certain time
+
+        //init timer to remove request from the data structures when not all acks are received after 5s
+        Timer timer = new Timer();
+        requestTimers.put(request.getRequestID(), timer);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                removeRequest(request.getRequestID());
+            }
+        }, 5000);
 
         logger.fine("Sending request " + request.getRequestID() + " to all servers...");
 
@@ -424,11 +433,14 @@ public class ServerProcess extends UnicastRemoteObject implements ClientServerIn
     }
 
     /**
-     * Removes the request with requestID from the pending requests (and acknowledgement counter)
+     * Removes the request with requestID from the pending requests (and acknowledgement counter).
+     * It also stops and removes the timer associated with the request
      *
      * @param requestID The id of the request to be removed
      */
     private void removeRequest(int requestID) {
+        requestTimers.get(requestID).cancel();
+        requestTimers.remove(requestID);
         pendingAcknowledgements.remove(requestID);
         pendingRequests.remove(requestID);
     }
