a70767e8d6888db3802de1e555940b5c5bd2e174
lfdversluis <laurens-versluis@hotmail.com>
2015-03-24 23:33:44 +0100

    changed the python file

--- experiment/response_times_dcs/process_data.py
+++ experiment/response_times_dcs/process_data.py
@@ -1,16 +1,27 @@
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+import pylab
 
-path = "C:/Users/Laurens/Desktop/response_times_dcs/*.txt"
+path = "/home/laurens/Documents/FDDG/experiment/response_times_dcs/*.txt"
 
 responses = []
 for fname in glob.glob(path):
+    print fname
     with open(fname) as f:
-		for line in f:
-			if line[2] == "response":
-				response.append(line[3])
+        for line in f:
+            if line.split()[1] == "response":
+                responses.append(int(line.split()[2]))
+                
+fig = plt.figure()
+fig.suptitle('Server-client response time', fontsize=14, fontweight='bold')
 
-plt.boxplot(response)
+ax1 = fig.add_subplot(111)
+
+ax1.set_xlabel('')
+ax1.set_ylabel('Resonse time (ms)')
+pylab.xticks([1], [''])
+
+plt.boxplot(responses)
 
 plt.show()
